68c361e137d2c3afc6972b796ed1ced5819533af
andrepujipurwadi <49966328+andrepujipurwadi@users.noreply.github.com>
2022-02-17 22:24:58 +0800

    Initial commit

---
+++ app/src/main/java/com/example/hitvol/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.hitvol
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+private lateinit var edtWidth: EditText
+private lateinit var edtHeigth: EditText
+private lateinit var edtLength: EditText
+private lateinit var btnCalculate: Button
+private lateinit var tvResult: TextView
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        edtWidth = findViewById(R.id.edt_width)
+        edtHeigth = findViewById(R.id.edt_height)
+        edtLength = findViewById(R.id.edt_length)
+        btnCalculate = findViewById(R.id.btn_calculate)
+        tvResult = findViewById(R.id.tv_result)
+
+        btnCalculate.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.btn_calculate) {
+            val inputLength = edtLength.text.toString().trim()
+            val inputWidth = edtWidth.text.toString().trim()
+            val inputHeight = edtHeigth.text.toString().trim()
+
+            val volume = inputLength.toDouble() * inputWidth.toDouble() * inputHeight.toDouble()
+            tvResult.text = volume.toString()
+
+    }
+}
+}
